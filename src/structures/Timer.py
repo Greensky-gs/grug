4f429c2a72f00e0eb94f2183784367a2758005dd
Greensky <david.hesliere@gmail.com>
2024-04-27 21:10:04 +0200

    chore: :bricks: Add a timer structure

---
+++ src/structures/Timer.py
@@ -0,0 +1,21 @@
+class Timer:
+    mod: int = 0
+    tracker: int = 0
+
+    def __init__(self, mod: int) -> None:
+        self.mod = mod
+    
+    def tick(self):
+        self.tracker = (self.tracker + 1) % self.mod
+        return self.valid
+    
+    def reset(self):
+        self.tracker = 0
+
+    @property
+    def valid(self):
+        return self.tracker == 0
+    
+    @property
+    def count(self):
+        return self.tracker
