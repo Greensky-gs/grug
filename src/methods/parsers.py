3d625709a828983971c6f6c06baacd56313b43e8
Greensky <david.hesliere@gmail.com>
2024-04-27 18:52:44 +0200

    chore: :bricks: Add direction parser

--- src/methods/parsers.py
+++ src/methods/parsers.py
@@ -1,4 +1,13 @@
 from utils.config import configs
+from typing import Literal
 
 def parsePos(x, y):
-    return (max(0, min(x, configs["WIDTH"])), max(0, min(y, configs["HEIGHT"])))
+    return (max(0, min(x, configs["WIDTH"] - 20)), max(0, min(y, configs["HEIGHT"] - 20)))
+def parseDirection(x, y) -> Literal["up", "down", "left", "right"]:
+    lastDir = None
+    if x != 0:
+        lastDir = "left" if x < 0 else "right"
+    else:
+        lastDir = "up" if y < 0 else "down"
+    
+    return lastDir
