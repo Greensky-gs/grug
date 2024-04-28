856df5eaad53fcc5125e567c2b2f0bf16a050448
Greensky <david.hesliere@gmail.com>
2024-04-28 21:47:39 +0200

    feat: :sparkles: Add scenes in Game

--- src/structures/Game.py
+++ src/structures/Game.py
@@ -4,13 +4,19 @@ from structures.Character import Character
 from utils.config import configs
 from methods.parsers import parseDirection, horizontal
 from methods.paths import Pathing
-from p5 import loadImage, image
+from p5 import loadImage, image, text
+from os import listdir
 
 class Game:
     player: Player.Player
     grugSprite: grug.Grug
     mapIndex = 0
-    paths = Pathing()
+    bgIndex = None
+    collisions = {
+        "paths": Pathing(f"./src/data/paths.json", 0),
+        "triggers": Pathing(f"./src/data/triggers.json", 1)
+    }
+    ready = False
 
     # Système de cache pour les arrières-plans
     bgCache = {}
@@ -25,6 +31,17 @@ class Game:
         self.player.moveTo(40, 716)
         self.grugSprite.moveTo(*self.player.grugPos("right"))
 
+        for file in listdir("./src/assets/bg"):
+            name = file.split(".")[0]
+            self.bgCache[name] = loadImage(f"./src/assets/bg/{name}.jpg")
+
+        self.ready = True
+
+    def setBgIndex(self, name):
+        self.bgIndex = name
+    def resetBgIndex(self):
+        self.bgIndex = None
+
     def movePlayer(self, *, x = 0, y = 0, moving = False):
         if moving:
             self.player.setTextureState("walk")
@@ -35,7 +52,7 @@ class Game:
 
         self.player.tickTexture()
 
-        self.player.move(x, y, self.paths)
+        self.player.move(x, y, paths=self.collisions["paths"])
         
         lastDir = parseDirection(x, y)
 
@@ -45,12 +62,20 @@ class Game:
         self.grugSprite.moveTo(*self.player.grugPos(self.grugSprite.lastDirection))
 
     def bgCaching(self):
-        if not self.mapIndex in self.bgCache:
-            bg = loadImage(f"./src/assets/maps/{self.mapIndex}.jpg")
-            self.bgCache[self.mapIndex] = bg
+        key = self.bgIndex if not self.bgIndex is None else self.mapIndex
+        path = f"./src/assets/maps/{self.mapIndex}.jpg" if self.bgIndex is None else f"./src/assets/bg/{self.bgIndex}.jpg"
+
+        if not key in self.bgCache:
+            bg = loadImage(path)
+            self.bgCache[key] = bg
 
             return bg
-        return self.bgCache[self.mapIndex]    
+        return self.bgCache[key]
+    def checkTriggers(self):
+        if self.collisions["triggers"].inPath(self.player.x, self.player.y):
+            scene = self.collisions["triggers"].getId(self.player.x, self.player.y)
+
+            self.setBgIndex(scene)
 
     def display(self):
         bg = self.bgCaching()
@@ -58,3 +83,6 @@ class Game:
 
         self.grugSprite.display()
         self.player.display()
+
+        self.checkTriggers()
+
