727210920267e9b66f6e926dbd1c5c5481e4bee7
Greensky <david.hesliere@gmail.com>
2024-04-28 21:46:29 +0200

    feat: :sparkles: Add collider path mode

--- src/methods/paths.py
+++ src/methods/paths.py
@@ -3,17 +3,28 @@ from typing import Dict, List
 
 class Pathing:
     paths: Dict[str, List[int]] = {}
+    mode: int
 
-    def __init__(self):
-        self.paths = load(open("src/data/paths.json"))
+    def __init__(self, path, mode):
+        self.paths = load(open(path, "r"))
+        self.mode = mode
     
     def inPath(self, x, y):
-        if not str(x) in self.paths:
-            return False
-        return y in self.paths[str(x)]
+        if self.mode == 0 or self.mode == 1:
+            if not str(x) in self.paths:
+                return False
+            return y in self.paths[str(x)] or str(y) in self.paths[str(x)]
     def closest(self, x, y):
-        closestX = min(self.paths.keys(), key=lambda k: abs(int(k) - x))
-        closestY = min(self.paths[closestX], key=lambda k: abs(int(k) - y))
+        if self.mode == 0:
+            closestX = min(self.paths.keys(), key=lambda k: abs(int(k) - x))
+            closestY = min(self.paths[closestX], key=lambda k: abs(int(k) - y))
 
-        return (int(closestX), int(closestY))
+            return (int(closestX), int(closestY))
+    def getId(self, x, y):
+        if self.mode != 1:
+            raise AttributeError("Les ids ne sont disponibles que dans le mode 1")
+        if not self.inPath(x, y):
+            return None
+        
+        return self.paths[str(x)][str(y)]
     
